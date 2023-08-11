27d06d2432a3f00ccc0c83cb94eab095ebc09108
lolkek1234567890 <dima2019mega@gmail.com>
2023-08-11 16:08:42 +0200

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -27,11 +27,22 @@ const Navbar = () => {
                     </button>
                 </div>
 
-                <button className="md:hidden block" onClick={toggleMenu}>
-                    {isOpen ? <Image src='/cancel.png' alt="close button" width={60} height={60} className="mr-3"/> : <Image src='/hambicon.png' alt='hamburger' width={80} height={80} />}   
+                <button className="md:hidden block transition-all duration-300" onClick={toggleMenu}>
+                    {isOpen ? <Image src='/cancel.png' alt="close button" width={50} height={50} className="mr-4"/> : <Image src='/hambicon.png' alt='hamburger' width={80} height={80} />}   
                 </button>
-                     
-              </nav>
+                </nav>
+                {isOpen ? (
+                    <div className="md:hidden flex flex-col bg-[#e7e1cd] z-10 transition ease-in-out duration-500 opacity-100  justify-center items-center">
+                        <ul className="w-full flex flex-col justify-center items-center text-[24px] mt-4 font-bold text-orange-400 gap-2">
+                        <li className="link">Contact</li>
+                        <li className="link">About</li>
+                        </ul>
+                        <button className="w-[150px] bg-orange-400 h-[50px] rounded-2xl hover:bg-orange-600 transition mb-10 mt-6">
+                        <p className="text-white font-bold text-[18px]">Time to order</p>
+                        </button>
+                    </div>
+                    ) :
+                null}
         </header>
       </>
   )
