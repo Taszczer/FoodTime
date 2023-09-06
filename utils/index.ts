aba453954938283cf848b062b4baaba412a69e9e
lolkek1234567890 <dima2019mega@gmail.com>
2023-09-06 21:04:08 +0200

    fixed fetch bug

--- utils/index.ts
+++ utils/index.ts
@@ -1,25 +1,34 @@
 import { ForFetch } from "@/types";
 
-//https://api.spoonacular.com/food/menuItems/search
-export default async function fetchFood({req, res}: ForFetch) {
-	try {
-	  const apiKey = "ea368868650145f9bcba1c8f3c8d35b0"; 
-	  const targetCalories = req.query.calories; 
-	  const targetMeal = req.query.food	
-  
-	  const response = await fetch(
-		`https://api.spoonacular.com/mealplanner/generate?apiKey=${apiKey}&targetMeal=${targetMeal}&targetCalories=${targetCalories}`
-	  );
-  
-	  if (!response.ok) {
-		throw new Error("Failed to fetch meal data");
-	  }
-  
-	  const data = await response.json();
-  
-	  res.status(200).json(data);
-	} catch (error) {
-	  console.error(error);
-	  res.status(500).json({ error: "An error occurred" });
-	}
+// https://api.spoonacular.com/food/menuItems/search
+export default async function fetchFood({ req, res }: ForFetch) {
+  try {
+    const apiKey = "ea368868650145f9bcba1c8f3c8d35b0";
+    const targetCalories = req.query.calories;
+    const targetMeal = req.query.food;
+
+    const response = await fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiKey}&targetMeal=${targetMeal}&targetCalories=${targetCalories}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch meal data");
+    }
+
+    const data = await response.json();
+
+    if (res) {
+      res.status(200).json(data);
+    } else {
+      console.error(`Response object ${res} is undefined`);
+    }
+  } catch (error) {
+    console.error(error);
+
+    if (res) {
+      res.status(500).json({ error: "An error occurred" });
+    } else {
+      console.error(`Response object ${res}is undefined`);
+    }
   }
+}
